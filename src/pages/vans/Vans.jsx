53a70ad5625693026080fdf3d1dd81d35f977350
Dastan Akatov <107542637+eB00k@users.noreply.github.com>
2023-08-13 00:10:40 +0600

    added filters to vans page

--- src/pages/vans/Vans.jsx
+++ src/pages/vans/Vans.jsx
@@ -4,28 +4,69 @@ import { VanService } from "../../services/vans.service";
 import VansCatalog from "../../components/vans-page/VansCatalog";
 import "./van.scss";
 
+const FILTER_OPTIONS = {
+  simple: false,
+  luxury: false,
+  rugged: false,
+};
+
 export default function Vans() {
-  const [vans, setVans] = useState([]);
+  const [allVans, setAllVans] = useState([]);
+  const [filters, setFilters] = useState(FILTER_OPTIONS);
+  const [displayedVans, setDisplayedVans] = useState([]);
 
   useEffect(() => {
-    const fetchData = async () => {
-      const data = await VanService.getVans();
-      console.log(data.vans);
-      setVans(data.vans);
-    };
     fetchData();
   }, []);
+
+  const fetchData = async () => {
+    try {
+      const data = await VanService.getVans();
+      setAllVans(data.vans);
+    } catch (err) {
+      console.error("Error fetching van data", err);
+    }
+  };
+
+  useEffect(() => {
+    filterVans();
+  }, [filters]);
+
+  function filterVans() {
+    const filteredVanList = allVans.filter((van) => filters[van.type]);
+    setDisplayedVans([...filteredVanList]);
+  }
+
+  function handleFilters(filter) {
+    setFilters((prevFilters) => ({
+      ...prevFilters,
+      [filter]: !prevFilters[filter],
+    }));
+  }
+
+  function clearFilter() {
+    setFilters(FILTER_OPTIONS);
+    fetchData();
+  }
 
   return (
     <div className="vans-container">
       <h2>Explore our van options</h2>
       <div className="vans-filter">
-        <span className="filter-btn">• Simple</span>
-        <span className="filter-btn">• Luxury</span>
-        <span className="filter-btn">• Rugged</span>
-        <span className="clear-btn">Clear Filters</span>
+        {Object.keys(FILTER_OPTIONS).map((filter) => (
+          <span
+            key={filter}
+            className={`filter-btn ${filters[filter] ? "active" : ""}`}
+            onClick={() => handleFilters(filter)}
+          >
+            • {filter[0].toUpperCase() + filter.slice(1)}
+          </span>
+        ))}
+        <span className="clear-btn" onClick={clearFilter}>
+          Clear Filters
+        </span>
       </div>
-      <VansCatalog vans={vans} />
+      <VansCatalog vans={displayedVans.length ? displayedVans : allVans} />
     </div>
   );
 }
