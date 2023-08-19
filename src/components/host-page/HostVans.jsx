82ba9a4df5405bdbfba81df600d43ffc58e1639d
Dastan Akatov <107542637+eB00k@users.noreply.github.com>
2023-08-20 00:52:28 +0600

    replaced await with (defer, Await, & Suspense)

--- src/components/host-page/HostVans.jsx
+++ src/components/host-page/HostVans.jsx
@@ -1,15 +1,16 @@
 import HostService from "../../services/host.service";
 import HostVansListCard from "./HostVansListCard";
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, defer, Await } from "react-router-dom";
+import { Suspense } from "react";
 import { requireAuth } from "../../utils/requireAuth";
 
 export async function loader({ request }) {
   await requireAuth(request);
   const fetchData = async () => {
     try {
-      const data = await HostService.getHostVans();
-      if (!data.length) throw new Error("There is no data");
-      return data;
+      const promiseData = HostService.getHostVans();
+      // if (!data.length) throw new Error("There is no data");
+      return defer({ data: promiseData });
     } catch (err) {
       console.log(err);
       throw err;
@@ -19,15 +20,24 @@ export async function loader({ request }) {
 }
 
 export default function HostVans() {
-  const hostVans = useLoaderData();
-  return (
-    <div className="host-page host-vans">
-      <h2 className="host-vans-title">Your Listed Vans</h2>
+  const promiseVans = useLoaderData();
+
+  function returnElements(hostVans) {
+    return (
       <div className="host-vans-list">
         {hostVans.map((van) => (
           <HostVansListCard van={van} key={van.id} />
         ))}
       </div>
+    );
+  }
+
+  return (
+    <div className="host-page host-vans">
+      <h2 className="host-vans-title">Your Listed Vans</h2>
+      <Suspense fallback={<h4>Loading...</h4>}>
+        <Await resolve={promiseVans.data}>{returnElements}</Await>
+      </Suspense>
     </div>
   );
 }
